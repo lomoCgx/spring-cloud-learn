0b183c8e3d7ab22e77f2fb45ceb560743fa34259
lomo <lomo_cgx@aliyun.com>
2018-01-04 20:14:34 +0800

    提交

--- cloud-parent/config-server/src/main/java/com/example/demo/DemoApplication.java
+++ cloud-parent/config-server/src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableConfigServer
+@EnableDiscoveryClient
 public class DemoApplication {
 
   public static void main(String[] args) {
